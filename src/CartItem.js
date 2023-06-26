b335d8f483a3c5e57cd647a12ce0f93ed6d1fc92
coolsachin2002@gmail.com <coolsachin2002@gmail.com>
2023-06-26 13:28:04 +0530

    added cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,45 +2,45 @@ import { render } from "@testing-library/react";
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            price:999,
-            title:"Phone",
-            qty:1,
-            img:""
+        this.state = {
+            price: 999,
+            title: "Phone",
+            qty: 1,
+            img: ""
         }
     }
-  
- increaseQty=()=> {
-    //set state form 1; 
-    // this.setState({
-    //     qty:this.state.qty+1
-    // })
 
-    //setState form 2; if require previous state we should use this;
-    this.setState((prevState) => {
-        return {
-            qty : prevState.qty+1
-        }
-    });
- }
- decreaseQty=()=>{
-    if (this.state.qty==0){
-        return;
-    }
-    this.setState((prevState)=>{
+    increaseQty = () => {
+        //set state form 1; 
+        // this.setState({
+        //     qty:this.state.qty+1
+        // })
+
+        //setState form 2; if require previous state we should use this;
+        this.setState((prevState) => {
             return {
-            qty:prevState.qty-1
-            
+                qty: prevState.qty + 1
+            }
+        });
+    }
+    decreaseQty = () => {
+        if (this.state.qty == 0) {
+            return;
         }
-      
-    })
- }
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+
+            }
+
+        })
+    }
 
 
     render() {
-        const {price,title,qty} = this.state;
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -53,17 +53,17 @@ class CartItem extends React.Component {
                     <div style={{ color: '#777' }}>Qty : {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" 
-                        className="action-icon" 
-                        src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
-                        onClick={this.increaseQty}></img>
-                        <img alt="decrease" 
-                        className="action-icon" 
-                        src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"
-                        onClick={this.decreaseQty}></img>
-                        <img alt="delete" 
-                        className="action-icon" 
-                        src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"></img>
+                        <img alt="increase"
+                            className="action-icon"
+                            src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+                            onClick={this.increaseQty}></img>
+                        <img alt="decrease"
+                            className="action-icon"
+                            src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"
+                            onClick={this.decreaseQty}></img>
+                        <img alt="delete"
+                            className="action-icon"
+                            src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"></img>
                     </div>
                 </div>
             </div>
