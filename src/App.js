637fc7dba06b2ec57cb99f696194c6947d541ed4
coolsachin2002@gmail.com <coolsachin2002@gmail.com>
2023-06-29 13:02:06 +0530

    fetched data from database

--- src/App.js
+++ src/App.js
@@ -2,36 +2,59 @@ import React from 'react';
 import CartItem from './CartItem';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products:[
-        {
-          price:99,
-          title:"Watch",
-          qty: 1,
-          img:"https://images.pexels.com/photos/277390/pexels-photo-277390.jpeg?cs=srgb&dl=pexels-pixabay-277390.jpg&fm=jpg",
-          id:1
-        },
-        {
-          price:999,
-          title:"Mobile Phone",
-          qty:10,
-          img:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-          id:2
-        },
-        {
-          price:999,
-          title:"laptop",
-          qty:4,
-          img:"https://cdn.mos.cms.futurecdn.net/CwpGTq5SSqWiafeEcWCnZJ-320-80.jpg",
-          id:3
-        }
-    ]
+        // {
+        //   price:99,
+        //   title:"Watch",
+        //   qty: 1,
+        //   img:"https://images.pexels.com/photos/277390/pexels-photo-277390.jpeg?cs=srgb&dl=pexels-pixabay-277390.jpg&fm=jpg",
+        //   id:1
+        // },
+        // {
+        //   price:999,
+        //   title:"Mobile Phone",
+        //   qty:10,
+        //   img:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+        //   id:2
+        // },
+        // {
+        //   price:999,
+        //   title:"laptop",
+        //   qty:4,
+        //   img:"https://cdn.mos.cms.futurecdn.net/CwpGTq5SSqWiafeEcWCnZJ-320-80.jpg",
+        //   id:3
+        // }
+    ],
+    loading:true,
     }
 }
+
+componentDidMount(){
+  firebase
+  .firestore()
+   .collection("products")
+    .get()
+     .then((snapshot)=>{
+      const products=snapshot.docs.map((doc)=>{
+        const data=doc.data();
+        data["id"]=doc.id;
+        return data;
+      })
+      this.setState({
+        products:products,
+        loading:false
+      })
+     })
+} 
+
+
 handleIncreaseQuantity= (product) => {
     const {products}=this.state;
     const index=products.indexOf(product);
@@ -78,7 +101,6 @@ const {products}=this.state;
    products.forEach((product)=>{
     count+=product.qty;
    })
-   console.log(count);
    return count;
 }
 
@@ -104,6 +126,7 @@ getTotalPrice =() =>{
         OnDecreaseQuantity={this.handleDecreaseQuantity}
         OnDeleteProduct={this.handleDeletingProduct}
         />
+        {loading && <h1>Loding Products ... </h1>}
         <div style={{fontSize:20,padding:10}}>TOTAL:{this.getTotalPrice()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from "firebase/compat/app";
 // import reportWebVitals from './reportWebVitals';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdQw0nTA8WmcsZI5y8OZ99tSAonJDdUpM",
+  authDomain: "cart-9e51f.firebaseapp.com",
+  projectId: "cart-9e51f",
+  storageBucket: "cart-9e51f.appspot.com",
+  messagingSenderId: "1009304499763",
+  appId: "1:1009304499763:web:7361edd4eae44189731546"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
